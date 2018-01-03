390fa9d1969e1c9dcb90658c71bfd1cfe32a9eaa
Justyna Jamróz <donrogal@gmail.com>
2018-01-03 22:52:12 +0100

    first commit

---
+++ Bekony/AppDelegate.swift
@@ -0,0 +1,95 @@
+//
+//  AppDelegate.swift
+//  Bekony
+//
+//  Created by Justyna Jamróz on 27/12/2017.
+//  Copyright © 2017 Konie. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, ESTMonitoringV2ManagerDelegate {
+
+    var window: UIWindow?
+
+    var monitoringManager: ESTMonitoringV2Manager!
+    
+    func showNotification(title: String, body: String) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = UNNotificationSound.default()
+        
+        let request = UNNotificationRequest(
+            identifier: "BeaconNotification", content: content, trigger: nil)
+        let center = UNUserNotificationCenter.current()
+        center.add(request, withCompletionHandler: nil)
+    }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        ESTConfig.setupAppID("ios-beacon-fmf", andAppToken: "eb3cb6e8134324785c522d2ecc0cd1ec")
+        self.monitoringManager = ESTMonitoringV2Manager(
+            desiredMeanTriggerDistance: 2.0, delegate: self)
+        self.monitoringManager.startMonitoring(forIdentifiers: [
+            "07d54301e7fbf5e0c1bdce7ee836b309"])
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            print("notifications allowed? = \(granted)")
+        }
+        return true
+    }
+    
+    func monitoringManager(_ manager: ESTMonitoringV2Manager,
+                           didEnterDesiredRangeOfBeaconWithIdentifier identifier: String) {
+        print("didEnter range of beacon \(identifier)")
+        
+        showNotification("Hello world", "Looks like you're near a beacon.")
+    }
+    func monitoringManager(_ manager: ESTMonitoringV2Manager,
+                           didExitDesiredRangeOfBeaconWithIdentifier identifier: String) {
+        print("didExit range of beacon \(identifier)")
+        
+        showNotification("Goodbye world", "You left the proximity of a beacon.")
+    }
+    func monitoringManager(_ manager: ESTMonitoringV2Manager,
+                           didDetermineInitialState state: ESTMonitoringState,
+                           forBeaconWithIdentifier identifier: String) {
+        // state codes: 0 = unknown, 1 = inside, 2 = outside
+        print("didDetermineInitialState '\(state)' for beacon \(identifier)")
+    }
+    
+    func monitoringManagerDidStart(_ manager: ESTMonitoringV2Manager) {
+        print("monitoringManagerDidStart")
+    }
+    func monitoringManager(_ manager: ESTMonitoringV2Manager,
+                           didFailWithError error: Error) {
+        print("monitoringManager didFailWithError: \(error.localizedDescription)")
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ Bekony/ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  Bekony
+//
+//  Created by Justyna Jamróz on 03/01/2018.
+//  Copyright © 2018 Konie. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
+#import <EstimoteSDK/EstimoteSDK.h>
